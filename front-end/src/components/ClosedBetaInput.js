ebab9ebe566491b5e68b1e1a54db7f6d15a00dd0
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2022-12-22 18:57:20 +0200

    cloesed beta added errors

--- front-end/src/components/ClosedBetaInput.js
+++ front-end/src/components/ClosedBetaInput.js
@@ -5,9 +5,12 @@ import { Button } from "@mui/material";
 import { firestore } from "../firebase";
 import { addDoc, collection } from "@firebase/firestore";
 import { useState } from "react";
+import Alert from "@mui/material/Alert";
 
 const ClosedBetaInput = () => {
   const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
   const ref = collection(firestore, "emails");
 
   function isValidEmail() {
@@ -26,51 +29,71 @@ const ClosedBetaInput = () => {
         console.log(e);
       }
       setEmail("");
+      setSuccess(true);
+      setTimeout(() => {
+        setSuccess(false);
+      }, 3000);
     } else {
-      window.alert("error");
+      setError(true);
+      setTimeout(() => {
+        setError(false);
+      }, 3000);
     }
   };
 
   const handleChange = (event) => {
+    setError(false);
     setEmail(event.target.value);
   };
 
   return (
-    <Box
-      component="form"
-      sx={{
-        "& > :not(style)": { m: 1, width: "25ch" },
-        display: "inline-flex",
-        marginTop: "2vh",
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField
-        id="outlined-name"
-        label="Email"
-        value={email}
-        onChange={handleChange}
-        color="secondary"
-        sx={{ backgroundColor: "white" }}
-      />
-      <Button
-        variant="contained"
-        size="large"
+    <div>
+      <Box
+        component="form"
         sx={{
-          width: "30ch",
-          marginTop: "1vh",
-          backgroundColor: "wheat",
-          color: "black",
-          "&.MuiButtonBase-root:hover": {
-            bgcolor: "wheat",
-          },
+          "& > :not(style)": { m: 1, width: "25ch" },
+          display: "inline-flex",
+          marginTop: "2vh",
         }}
-        onClick={handleClick}
+        noValidate
+        autoComplete="off"
       >
-        Sign Up
-      </Button>
-    </Box>
+        <TextField
+          id="outlined-name"
+          label="Email"
+          value={email}
+          onChange={handleChange}
+          color="secondary"
+          sx={{ backgroundColor: "white" }}
+        />
+        <Button
+          variant="contained"
+          size="large"
+          sx={{
+            width: "30ch",
+            marginTop: "1vh",
+            backgroundColor: "wheat",
+            color: "black",
+            "&.MuiButtonBase-root:hover": {
+              bgcolor: "wheat",
+            },
+          }}
+          onClick={handleClick}
+        >
+          Sign Up
+        </Button>
+      </Box>
+      {error && (
+        <Alert sx={{ width: "10vw", marginLeft: "20vw" }} severity="error">
+          Invalid Email
+        </Alert>
+      )}
+      {success && (
+        <Alert sx={{ width: "15vw", marginLeft: "15vw" }} severity="success">
+          Thank you for signing up for our BETA
+        </Alert>
+      )}
+    </div>
   );
 };
 export default ClosedBetaInput;
